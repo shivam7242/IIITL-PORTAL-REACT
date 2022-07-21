498e0787ac9d63916165ab4dcb7ad345415b5816
notabhishek22 <lit2021004@iiitl.ac.in>
2022-07-21 15:48:20 +0530

    Added Schemas for Database

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
-import logo from './logo.svg';
 import './App.css';
 import { Dashboard } from './components/Dashboard';
 import { LandingPage } from './components/LandingPage';
