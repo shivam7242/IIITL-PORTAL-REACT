57cdbc545fac6b4f61b3f54e1b54dc426f29ed6e
notabhishek22 <lit2021004@iiitl.ac.in>
2022-07-28 21:23:55 +0530

    Edited the Schemas folder

--- Backend/app.js
+++ Backend/app.js
@@ -1,61 +1,52 @@
-const express= require("express");
-const bodyParser= require("body-parser");
-const mongoose= require("mongoose");
-const cors=require("cors");
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
 const Users = require('./schema/User');
-const app= express();
+const app = express();
 app.use(cors());
 
-
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb+srv://kanishk_0012:kanukrishna@cluster0.jvwy6.mongodb.net/Users_Data?retryWrites=true&w=majority',{
-    useNewUrLParser:true,
-    useUnifiedTopology:true
-});  
-
-var db= mongoose.connection;
-db.on('error',()=>console.log("error in connecting to database"));
-db.once('open',()=>console.log("connected to db"))
-
-app.post("/",async function(req,res){
-    let Email= req.body.email;
-    let Password= req.body.password;
-    
-    // let data={"Name": "Testuser2", "Email": "Test_user2@gmail.com", "Password": "Test@2"}
-    // db.collection('users').insertOne(data,(err,collection)=>{
-    //     if(err){
-    //         throw err;
-    //     }
-    //     console.log("record inserted successfully")
-    // })
-
-    // const newUser = new Users({"Name": "Testuser1", "Email": "Test_user1@gmail.com", "Password": "Test@1"}).save();
-
-  let k= await Users.findOne({ Email })
-  if(k) {
-     if(k.Password==Password)
-     {
-      res.status(200).json(k);
-     }
-     else
-     res.status(400).json({error:"Wrong Password"})
-  }
-  else
+mongoose.connect(
+  'mongodb+srv://kanishk_0012:kanukrishna@cluster0.jvwy6.mongodb.net/Users_Data?retryWrites=true&w=majority',
   {
-    res.status(400).json({ error: "User doesn't exist"});
+    useNewUrLParser: true,
+    useUnifiedTopology: true,
   }
-console.log(k);
-})
+);
 
+var db = mongoose.connection;
+db.on('error', () => console.log('error in connecting to database'));
+db.once('open', () => console.log('connected to db'));
 
+app.post('/', async function (req, res) {
+  let Email = req.body.email;
+  let Password = req.body.password;
 
+  // let data={"Name": "Testuser2", "Email": "Test_user2@gmail.com", "Password": "Test@2"}
+  // db.collection('users').insertOne(data,(err,collection)=>{
+  //     if(err){
+  //         throw err;
+  //     }
+  //     console.log("record inserted successfully")
+  // })
 
+  // const newUser = new Users({"Name": "Testuser1", "Email": "Test_user1@gmail.com", "Password": "Test@1"}).save();
 
-
-app.listen(4000,function(){ 
-console.log("The server is running on port 4000");
+  let k = await Users.findOne({ Email });
+  if (k) {
+    if (k.Password === Password) {
+      res.status(200).json(k);
+    } else res.status(400).json({ error: 'Wrong Password' });
+  } else {
+    res.status(400).json({ error: "User doesn't exist" });
+  }
+  console.log(k);
 });
 
+app.listen(4000, function () {
+  console.log('The server is running on port 4000');
+});
--- Backend/schema/User.js
+++ Backend/schema/User.js
@@ -1,27 +1,54 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-
-const UserSchema = new mongoose.Schema({
-  Email: {
-    type: String,
-    required: true,
+const UserSchema = new mongoose.Schema(
+  {
+    Email: {
+      type: String,
+      required: true,
+    },
+    Password: {
+      type: String,
+      required: true,
+    },
+    Name: {
+      type: String,
+      required: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    gender: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    batch: {
+      type: Number,
+      required: true,
+    },
+    semester: {
+      type: Number,
+      required: true,
+    },
+    branch: {
+      type: String,
+      required: true,
+    },
+    courses: {
+      type: Array,
+    },
   },
-  Password: {
-    type: String,
-    required: true,
-  },
-  Name: {
-    type: String,
-    required: true,
-  },
- 
-},
-{
-  collection:"Users"
-}
-
-
-
+  {
+    collection: 'Users',
+  }
 );
 
-module.exports = new mongoose.model("Users", UserSchema);
+module.exports = new mongoose.model('Users', UserSchema);
